4710a0dfb58f4d1f9f05837dfaacabd138415034
Jeong77777 <ttletter7@naver.com>
2022-10-18 17:37:48 +0900

    221018_1736

--- ChatProgramForClient/widget.h
+++ ChatProgramForClient/widget.h
@@ -2,10 +2,31 @@
 #define WIDGET_H
 
 #include <QWidget>
+#include <QDataStream>
+
+class QTcpSocket;
 
 QT_BEGIN_NAMESPACE
 namespace Ui { class Widget; }
 QT_END_NAMESPACE
+
+typedef enum {
+    Chat_Login,
+    Chat_In,
+    Chat_Talk,
+    Chat_Close,
+    Chat_LogOut,
+    Chat_Invite,
+    Chat_KickOut,
+    Chat_FileTrans_Start,
+    Chat_FileTransfer,
+    Chat_FileTrans_End,
+} Chat_Status;
+
+typedef struct {
+    Chat_Status type;
+    char data[1020];
+} chatProtocolType;
 
 class Widget : public QWidget
 {
@@ -15,7 +36,17 @@ public:
     Widget(QWidget *parent = nullptr);
     ~Widget();
 
+private slots:
+    void connectToServer( );
+    void receiveData( );			// 서버에서 데이터가 올 때
+    void sendData( );			// 서버로 데이터를 보낼 때
+    void disconnect( );
+
 private:
     Ui::Widget *ui;
+
+    void closeEvent(QCloseEvent*) override;
+
+    QTcpSocket *clientSocket;		// 클라이언트용 소켓
 };
 #endif // WIDGET_H
